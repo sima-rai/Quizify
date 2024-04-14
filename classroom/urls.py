63f385c075b3769d1bb5e41a4f382347908b30f7
seema <raiseema760@gmail.com>
2024-04-14 15:10:29 +0100

    take quiz implementation

--- classroom/urls.py
+++ classroom/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
         path('', students.QuizListView.as_view(), name='quiz_list'),
         path('taken/', students.TakenQuizListView.as_view(), name='taken_quiz_list'),
         path('interests/', students.StudentInterestsView.as_view(), name='student_interests'),
+        path('quiz/<int:pk>/', students.take_quiz, name='take_quiz'),
+
 
 
        
--- classroom/views/forms.py
+++ classroom/views/forms.py
@@ -88,6 +88,22 @@ class StudentSignUpForm(UserCreationForm):
         student.interests.add(*self.cleaned_data.get('interests'))
         return user
 
+class TakeQuizForm(forms.ModelForm):
+    answer = forms.ModelChoiceField(
+        queryset=Answer.objects.none(),
+        widget=forms.RadioSelect(),
+        required=True,
+        empty_label=None)
+
+    class Meta:
+        model = StudentAnswer
+        fields = ('answer', )
+
+    def __init__(self, *args, **kwargs):
+        question = kwargs.pop('question')
+        super().__init__(*args, **kwargs)
+        self.fields['answer'].queryset = question.answers.order_by('text')
+
 
 
 
