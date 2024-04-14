13cc983f22c529f61bf903bc35508996a1891008
seema <raiseema760@gmail.com>
2024-04-14 13:06:53 +0100

    teacher landing page that shows quiz list view

--- classroom/views/teachers.py
+++ classroom/views/teachers.py
@@ -39,6 +39,13 @@ class QuizListView(ListView):
     context_object_name = 'quizzes'
     template_name = 'classroom/teachers/quiz_change_list.html'
 
+    def get_queryset(self):
+        queryset = self.request.user.quizzes \
+            .select_related('subject') \
+            .annotate(questions_count=Count('questions', distinct=True)) \
+            .annotate(taken_count=Count('taken_quizzes', distinct=True))
+        return queryset
+
 
 
 @method_decorator([login_required, teacher_required], name='dispatch')
